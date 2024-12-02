943ad415c312a7f2c8fdda910b609400fb0753a1
Shipragit <shiva291291@gmail.com>
2024-12-02 17:59:40 +0530

    Update ExtractValues.py

--- ExtractValues.py
+++ ExtractValues.py
@@ -1,21 +1,16 @@
 import requests
 
-# Set the request parameters
-url = 'https://brightspeedtsmqa1.service-now.com/api/sn_cicd/testsuite/results/4a81bf581b521e107ba2ec64604bcb0f'
-
-# Eg. User name="admin", Password="admin" for this code sample.
+# URL and authentication info
+url = 'https://brightspeedtsmqa1.service-now.com/api/sn_cicd/testsuite/results/875529951b4a1a103e6ccae2604bcb9e'
 user = 'shivakumara.appaji@infinite.com'
 pwd = 'Pratheja@123'
+headers = {"Content-Type": "application/json", "Accept": "application/json"}
 
-# Set proper headers
-headers = {"Content-Type":"application/json","Accept":"application/json"}
+response = requests.get(url, auth=(user, pwd), headers=headers)
 
-# Do the HTTP request
-response = requests.get(url, auth=(user, pwd), headers=headers )
-
-# Check for HTTP codes other than 200
-if response.status_code != 200: 
-    print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())
+# Check if the response is successful
+if response.status_code != 200:
+    print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.json())
     exit()
 
 # Parse the JSON response
